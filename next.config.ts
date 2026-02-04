5f0648aff98e62e77b632c21ea0d9831402eacc3
ftobe-maths974.fr <florian.tobe@maths974.fr>
2026-02-04 12:34:47 +0400

    v0.1 ready

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Active l'exportation statique
+  images: {
+    unoptimized: true, // GitHub Pages ne supporte pas l'optimisation d'images native de Next.js
+  },
 };
 
 export default nextConfig;
